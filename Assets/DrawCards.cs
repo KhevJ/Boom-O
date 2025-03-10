5213bc3337489c2df98fe67bfc1ef08ef16f60e7
hridikabanik02 <109242454+hridikabanik02@users.noreply.github.com>
2025-03-10 14:39:28 -0600

    Tried to add more prefabs and draw cards in Player hands

--- Assets/DrawCards.cs
+++ Assets/DrawCards.cs
@@ -1,11 +1,11 @@
 using System.Collections;
-using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class DrawCard : MonoBehaviour
 {
     public List<GameObject> cardPrefabs; // List of all card prefabs
-    //public Transform playerHand;  // Where drawn cards will appear
+    public Transform playerHand; // Assign PlayerCards object in Inspector
 
     void OnMouseDown() // Detect clicks on the draw pile
     {
@@ -17,12 +17,27 @@ public class DrawCard : MonoBehaviour
         if (cardPrefabs.Count > 0) // Check if deck is not empty
         {
             int randomIndex = Random.Range(0, cardPrefabs.Count); // Pick a random card
-            GameObject selectedCard = cardPrefabs[randomIndex]; // Get the prefab
+            GameObject selectedCard = Instantiate(cardPrefabs[randomIndex], playerHand); // Parent to PlayerCards
 
-            GameObject newCard = Instantiate(selectedCard);
-            newCard.transform.position = new Vector3(-6, 3, 0);
+            // Get RectTransform since we are using UI
+            RectTransform cardRect = selectedCard.GetComponent<RectTransform>();
 
-            cardPrefabs.RemoveAt(randomIndex); // Remove drawn card from deck
+            if (cardRect != null)
+            {
+                int cardCount = playerHand.childCount - 1; // Count existing cards
+                float overlapAmount = -60f; //Negative for overlap (adjust value if needed)
+
+                // Position cards with slight overlap
+                cardRect.anchoredPosition = new Vector2(cardCount * overlapAmount, 0);
+                cardRect.localScale = Vector3.one; // Ensure correct size
+            }
+            else
+            {
+                Debug.LogError("New card does not have a RectTransform!");
+            }
+
+            // Remove the drawn card from the deck
+            cardPrefabs.RemoveAt(randomIndex);
         }
         else
         {
