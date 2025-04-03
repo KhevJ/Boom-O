0cd5f95cdcae2cf788c0863c2a336d64982873ee
sumerah.rowshan <sumerahrowshan15.03@gmail.com>
2025-04-02 23:41:01 -0600

    up

--- server/index4.js
+++ server/index4.js
@@ -321,11 +321,11 @@ const servers = [
 
 const links = new Map();
 
-links.set(3000, "http://localhost:3000/ring"); 
-links.set(3001, "http://localhost:3001/ring");
-links.set(3002, "http://localhost:3002/ring");
-links.set(3003, "http://localhost:3003/ring");
-links.set(3004, "http://localhost:3004/ring");
+links.set(3000, "https://d8c6-2604-3d09-e7e-b800-84a6-c14a-2091-2650.ngrok-free.app/ring"); 
+links.set(3001, "https://cf6c-2604-3d09-d80-b600-17b-981f-74b6-c25d.ngrok-free.app/ring");
+links.set(3002, "https://7ebf-2604-3d09-e7e-b800-8544-e02d-6756-906e.ngrok-free.app/ring");
+links.set(3003, "https://85ca-174-0-240-161.ngrok-free.app/ring");
+links.set(3004, "https://9ede-2604-3d09-d75-8900-9c42-7da9-6afa-5688.ngrok-free.app/ring");
 
 
 
